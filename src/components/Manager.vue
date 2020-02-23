3cb461798b68c50e4a4eaaaf550021a515d02c8c
jelneo <neohuilingjelena@gmail.com>
2020-02-23 12:59:45 +0800

    Add buttons for actions

--- src/components/Manager.vue
+++ src/components/Manager.vue
@@ -1,7 +1,24 @@
 <template>
-    <container>
-        Manager
-    </container>
+    <v-container>
+        <v-toolbar class="transparent" align="center" justify="center">
+            <v-col>
+                <v-btn class="grey darken-3"> Add promotion </v-btn>
+            </v-col>
+
+            <v-col>
+                <v-btn class="grey darken-3"> Add/delete restaurant and staff </v-btn>
+            </v-col>
+
+            <v-col>
+                <v-btn class="grey darken-3"> Add/Delete rider </v-btn>
+            </v-col>
+
+            <v-col>
+                <v-btn class="grey darken-3"> View summary </v-btn>
+            </v-col>
+        </v-toolbar>
+
+    </v-container>
 </template>
 
 <script>
