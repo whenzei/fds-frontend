81096872e00d45f9c5574e98bb329f928e75bb21
jelneo <neohuilingjelena@gmail.com>
2020-02-26 01:57:37 +0800

    Change to tabs

--- src/components/Manager.vue
+++ src/components/Manager.vue
@@ -1,28 +1,27 @@
 <template>
-    <v-container>
-        <v-toolbar class="transparent" align="center" justify="center">
-            <v-col>
-                <v-btn @click="addPromotion" class="grey darken-3"> Add promotion </v-btn>
-            </v-col>
-
-            <v-col>
-                <v-btn @click="updateRestStaff" class="grey darken-3"> Add/delete restaurant and staff </v-btn>
-            </v-col>
-
-            <v-col>
-                <v-btn @click="addDeleteRider" class="grey darken-3"> Add/Delete rider </v-btn>
-            </v-col>
-
-            <v-col>
-                <v-btn @click="viewSummary" class="grey darken-3"> View summary </v-btn>
-            </v-col>
+    <v-container fluid>
+        <v-toolbar flat color="orange darken-3" dark>
+            <v-toolbar-title>Manager Workspace</v-toolbar-title>
         </v-toolbar>
+        <v-tabs vertical color="orange darken-3" left>
+            <v-tab>Add/delete restaurant and staff</v-tab>
+            <v-tab>Add/Delete rider</v-tab>
+            <v-tab>Add Promotion</v-tab>
+            <v-tab>View Summary</v-tab>
 
-            <v-container>
-                <div v-show="showSummary"><ViewSummary /></div>
-                <div v-show="showPromo"><AddPromotion /></div>
-                
-            </v-container>
+            <v-tab-item>
+                <v-card-text>Tab 1</v-card-text>
+            </v-tab-item>
+            <v-tab-item>
+                <v-card-text>Tab 2</v-card-text>
+            </v-tab-item>
+            <v-tab-item>
+                <AddPromotion />
+            </v-tab-item>
+            <v-tab-item>
+                <ViewSummary />
+            </v-tab-item>
+        </v-tabs>         
     </v-container>
 </template>
 
@@ -36,25 +35,6 @@ export default {
         AddPromotion
     },
     data: () => ({
-        showSummary: false,
-        showPromo: false
     }),
-    methods: {
-        addPromotion() {
-            this.showPromo = true;
-            this.showSummary = false;
-        },
-        updateRestStaff() {
-            // this.showSummary = "Update restaurant and staff";
-        },
-        addDeleteRider() {
-            // this.showSummary = "Add/delete rider";
-        },
-        viewSummary() {
-            this.showSummary = true;
-            this.showPromo = false;
-        },
-    }
-    
 }
 </script>
