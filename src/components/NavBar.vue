f0ef34d63c6a23d7165f9fa5abf0a8a1e6aebb4c
whenzei <e0032088@u.nus.edu>
2020-03-17 21:20:54 +0800

    refactor login and add logout feature

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,10 +1,30 @@
 <template>
-    <v-app-bar app color="black" dark>
-      <router-link to="/">
-        <div class="d-flex align-center">
-          <v-icon large class="orange--text text--darken-1">mdi-bike-fast</v-icon>
-          <span class="ml-2 font-weight-medium orange--text text--darken-1">FDS</span>
-        </div>
-      </router-link>
-    </v-app-bar>
+  <v-app-bar app color="black" dark>
+    <router-link to="/">
+      <div class="d-flex align-center">
+        <v-icon large class="orange--text text--darken-1">mdi-bike-fast</v-icon>
+        <span class="ml-2 font-weight-medium orange--text text--darken-1">FDS</span>
+      </div>
+    </router-link>
+    <v-spacer></v-spacer>
+    <v-btn v-if="checkLoginStatus" outlined color="orange" @click="logout">Logout</v-btn>
+  </v-app-bar>
 </template>
+
+<script>
+export default {
+  data: () => ({
+  }),
+  computed: {
+    checkLoginStatus() {
+      return this.$store.getters.isLoggedIn;
+    }
+  },
+  methods: {
+    async logout() {
+      await this.$store.dispatch('logout');
+      this.$router.push('/');
+    }
+  }
+};
+</script>
--- src/main.js
+++ src/main.js
@@ -3,8 +3,10 @@ import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import router from './router'
 import store from './store'
+import axios from 'axios'
 
 Vue.config.productionTip = false
+axios.defaults.baseURL = process.env.VUE_APP_BACKEND_URL
 
 new Vue({
   vuetify,
