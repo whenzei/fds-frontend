0cb661ed0dd46cf9408a16edb6904acac24b99b7
MuhdNurKamal <mnkamal95@gmail.com>
2020-03-18 12:55:25 +0800

    Add nav guard for each user role

--- src/router/index.js
+++ src/router/index.js
@@ -5,12 +5,19 @@ import Customer from '@/components/Customer'
 import Rider from '@/components/Rider'
 import Staff from '@/components/Staff'
 import Manager from '@/components/Manager'
+import ErrorLanding from '@/components/ErrorLanding'
+import store from '../store'
 
 Vue.use(VueRouter);
 
-export default new VueRouter({
+const router = new VueRouter({
     mode: 'history',
     routes: [
+        {
+            path: '/err',
+            name: 'ErrorLanding',
+            component: ErrorLanding,
+        },
         {
             path: '/',
             name: 'Landing',
@@ -35,6 +42,18 @@ export default new VueRouter({
             path: '/manager',
             name: 'Manager',
             component: Manager,
+            beforeEnter: (to, from, next) => {
+                if (store.getters.role != 'Manager') {
+                    router.push({ name: 'ErrorLanding' })
+                }
+                else { next() }
+            }
         }
     ]
 })
+
+router.beforeEach((to, from, next) => {
+    if (to.name !== 'Landing' && !store.getters.isLoggedIn) router.push({ name: 'Landing' })
+    else { next() }
+})
+export default router
