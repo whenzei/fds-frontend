0d6d8769ed24d7ed48bf08c5593a24f441238b21
MuhdNurKamal <mnkamal95@gmail.com>
2020-04-02 21:58:51 +0800

    Add add slot button

--- src/components/Rider/UpdatePTSchedule.vue
+++ src/components/Rider/UpdatePTSchedule.vue
@@ -5,38 +5,51 @@
       <v-card-title>End: {{end.format('LL')}}</v-card-title>
     </v-card>
     <v-form ref="form" v-model="valid" lazy-validation>
+      <v-list>
+        <v-list-item v-for="dailySchedule in dailySchedules" :key="dailySchedule.day">
+          <v-list-item-group>
+            <v-list-item-content>{{start.clone().add(dailySchedule.day, 'day').format('LL')}}</v-list-item-content>
+            <v-list-item v-for="(slot, i) in dailySchedule.slots" :key="i" dense>
+              {{slot.startTime}} - {{slot.endTime}}
+            </v-list-item>
+            <v-btn @click="addSlot(dailySchedule.day)" class="mx-2" fab dark color="orange">
+              <v-icon dark>mdi-plus</v-icon>
+            </v-btn>
+          </v-list-item-group>
+        </v-list-item>
+      </v-list>
     </v-form>
   </v-container>
 </template>
 <script>
-import { postScheduleUpdate } from "../../helpers/rider";
 import moment from "moment";
 export default {
   props: ["year", "week"],
   data: function() {
     const nextMonthDate = new Date();
     nextMonthDate.setMonth(nextMonthDate.getMonth() + 1);
-    const start = moment(`${this.year}-W${this.week}-1`)
-    const end = moment(`${this.year}-W${this.week}-7`)
+    const start = moment(`${this.year}-W${this.week}-1`);
+    const end = moment(`${this.year}-W${this.week}-7`);
     return {
       month: nextMonthDate.getMonth() + 1,
       start,
       end,
-      months: [
-        "January",
-        "February",
-        "March",
-        "April",
-        "May",
-        "June",
-        "July",
-        "August",
-        "September",
-        "October",
-        "November",
-        "December"
-      ],
-      valid: true
+      valid: true,
+      dailySchedules: [
+        {
+          day: 0,
+          slots: [
+            {
+              startTime: 10,
+              endTime: 22
+            }
+          ]
+        },
+        {
+          day: 1,
+          slots: []
+        }
+      ]
     };
   },
   methods: {
@@ -46,26 +59,8 @@ export default {
     reset() {
       this.$refs.form.reset();
     },
-    applySchedule() {
-      if (!this.validate()) {
-        return;
-      }
-      const payLoad = {
-        year: this.year,
-        month: this.month,
-        startDayOfMonth: this.startDayOfMonth,
-        firstDayShiftId: this.firstDayShiftId,
-        secondDayShiftId: this.secondDayShiftId,
-        thirdDayShiftId: this.thirdDayShiftId,
-        fourthDayShiftId: this.fourthDayShiftId,
-        fifthDayShiftId: this.fifthDayShiftId
-      };
-      postScheduleUpdate(payLoad)
-        .then(() => {
-          alert("Updated");
-          this.$router.push("/rider/schedule");
-        })
-        .catch(err => alert(err));
+    addSlot(day) {
+      console.log(day);
     }
   },
   created: async function() {}
