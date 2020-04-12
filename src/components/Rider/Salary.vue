c93cb1fe7a4d2950cefd8ef1196e132db6f3c14a
MuhdNurKamal <mnkamal95@gmail.com>
2020-04-12 22:22:16 +0800

    Integrate salary with backend

--- src/components/Rider/Salary.vue
+++ src/components/Rider/Salary.vue
@@ -1,55 +1,89 @@
 <template>
   <v-container fluid>
-    <v-select :items="years" label="Year" v-model="selectedYear"></v-select>
+    <v-select :items="years" label="Year" v-model="selectedYear" v-on:change="fetchSalaryInfo"></v-select>
     <v-data-table
       :headers="headers"
-      :items="salaryInfo[selectedYear]"
+      :items="tableItems"
       :items-per-page="itemsPerPage"
       hide-default-footer
       class="elevation-1"
-    ></v-data-table>
+    >
+      <template v-slot:item.baseSalary="{ item }">
+        <span>{{formatCurrency(item.baseSalary)}}</span>
+      </template>
+      <template v-slot:item.commission="{ item }">
+        <span>{{formatCurrency(item.commission)}}</span>
+      </template>
+      <template v-slot:item.totalSalary="{ item }">
+        <span>{{formatCurrency(item.totalSalary)}}</span>
+      </template>
+    </v-data-table>
   </v-container>
 </template>
 
 <script>
+const { getSalaryInfo } = require("../../helpers/rider");
+const { formatCurrency } = require("../../helpers/format");
+const _ = require("lodash");
 export default {
   data: function() {
+    const thisYear = new Date().getFullYear();
+    const fullTimeHeaders = [
+      { text: "Month Number", value: "monthNumber" },
+      { text: "Month", value: "month" },
+      { text: "Base Salary (SGD)", value: "baseSalary" },
+      { text: "Commission (SGD)", value: "commission" },
+      { text: "Total Salary (SGD)", value: "totalSalary" },
+      { text: "Start Date", value: "startDate" },
+      { text: "End Date", value: "endDate" },
+      { text: "Payout Date", value: "payOutDate" }
+    ];
+    const partTimeHeaders = [
+      { text: "Week Number", value: "weekNumber" },
+      { text: "Base Salary (SGD)", value: "baseSalary" },
+      { text: "Commission (SGD)", value: "commission" },
+      { text: "Total Salary (SGD)", value: "totalSalary" },
+      { text: "Start Date", value: "startDate" },
+      { text: "End Date", value: "endDate" },
+      { text: "Payout Date", value: "payOutDate" }
+    ];
     return {
-      selectedYear: "",
-      years: [2012, 2013],
-      headers: [
-        { text: "Month Number", value: "monthNumber" },
-        { text: "Month", value: "month" },
-        { text: "Salary (SGD)", value: "salary" }
-      ],
-      salaryInfo: {
-        2012: [
-          {
-            monthNumber: 1,
-            month: "Jan",
-            salary: 5000 / 100
-          },
-          {
-            monthNumber: 3,
-            month: "Mar",
-            salary: 700 / 100
-          }
-        ],
-        2013: [
-          {
-            monthNumber: 2,
-            month: "Feb",
-            salary: 5000 / 100
-          },
-          {
-            monthNumber: 10,
-            month: "Oct",
-            salary: 700 / 100
-          }
-        ]
-      },
+      selectedYear: thisYear,
+      years: _.range(2018, thisYear + 1),
+      partTimeHeaders,
+      fullTimeHeaders,
+      headers: fullTimeHeaders,
+      salaryInfo: {},
       itemsPerPage: 12
     };
+  },
+  methods: {
+    formatCurrency,
+    fetchSalaryInfo() {
+      getSalaryInfo(this.selectedYear)
+        .then(salaryInfo => {
+          this.salaryInfo = salaryInfo;
+          if ("months" in salaryInfo) {
+            this.headers = this.fullTimeHeaders;
+          } else {
+            this.headers = this.partTimeHeaders;
+          }
+        })
+        .catch(e => {
+          alert(e);
+        });
+    }
+  },
+  created() {
+    this.fetchSalaryInfo();
+  },
+  computed: {
+    tableItems() {
+      if ("months" in this.salaryInfo) {
+        return this.salaryInfo.months;
+      }
+      return this.salaryInfo.weeks;
+    }
   }
 };
 </script>
